3f033b8e9229b004467997a954f3e40428db8cb2
abhyas21 <abhyas2006@gmail.com>
2026-04-16 21:47:51 +0530

    Added neagative-num.py

---
+++ neagative-num.py
@@ -0,0 +1,9 @@
+***Write a program that reads a number and checks if the given number is a negative number.
+Note
+Negative numbers are numbers that are less than zero.
+Input
+The input will be a single line containing an integer.
+Output
+The output should be a single line containing a boolean. True should be printed if the given number is a negative number, otherwise False should be printed.***
+a=input()
+print(int(a)<0)
